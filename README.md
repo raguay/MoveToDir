ab4e592a086795b2167c1f746799def2d86503d7
Richard Guay <raguay@customct.com>
2017-03-08 16:07:40 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+## MoveToDir
+
+Plugin for [fman.io](https://fman.io) to move a selection of files to a directory. If the directory doesn't exist, create it and move the files.
+
+Install by uploading "MoveToDir" to your [data directory](https://fman.io/docs/customizing-fman)`/Plugins`.
+
+### Usage
+
+Select one or more files/directories and press **Shift+m** (I'm using my Vim Keymappings). Fman will then ask for a directory name. If the directory doesn't exist, create it and move the files there. Otherwise, just move the files.
+
+**Warning**: Currently it runs in the same process/thread so be aware that running properties on a large dir will cause the UI to hang while calculating size
+
+### Features
+
+ - Creates named directory if it doesn't exist
+ - Moves the files to that directory
---
+++ movetodir/__init__.py
@@ -0,0 +1,29 @@
+from fman import DirectoryPaneCommand, show_alert, show_prompt
+import os, shutil
+
+class MoveToDir(DirectoryPaneCommand):
+    def __call__(self):
+        selected_files = self.pane.get_selected_files()
+        output = ""
+        if len(selected_files) >= 1 or (len(selected_files) == 0 and self.get_chosen_files()):
+            if len(selected_files) == 0 and self.get_chosen_files():
+                selected_files.append(self.get_chosen_files()[0])
+            dirPath = os.path.dirname(selected_files[0]) + os.sep
+            ndnam,okay = show_prompt("Directory Name?")
+            if okay:
+                numf = 0
+                newdirp = dirPath + ndnam + os.sep
+                if not os.path.isdir(newdirp):
+                    os.mkdir(newdirp)
+                for filep in selected_files:
+                    head, tail = os.path.split(filep)
+                    filename = tail or os.path.basename(head)
+                    shutil.move(dirPath + filename,newdirp + filename)
+                    numf += 1
+                output += str(numf) + " items were moved!"
+            else:
+                output = "User Canceled!"
+        else:
+            output += "No files or directories selected"
+
+        show_alert(output)
