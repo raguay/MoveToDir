a3b5b8fc8d99cb760909279268468af1d9a4bef9
Richard Guay <raguay@customct.com>
2017-12-01 13:43:44 -0500

    Updating for new file system api

--- movetodir/__init__.py
+++ movetodir/__init__.py
@@ -1,5 +1,9 @@
 from fman import DirectoryPaneCommand, show_alert, show_prompt
-import os, shutil
+import os
+import shutil
+from fman.url import as_human_readable
+from fman.url import as_url
+
 
 class MoveToDir(DirectoryPaneCommand):
     def __call__(self):
@@ -8,7 +12,7 @@ class MoveToDir(DirectoryPaneCommand):
         if len(selected_files) >= 1 or (len(selected_files) == 0 and self.get_chosen_files()):
             if len(selected_files) == 0 and self.get_chosen_files():
                 selected_files.append(self.get_chosen_files()[0])
-            dirPath = os.path.dirname(selected_files[0]) + os.sep
+            dirPath = os.path.dirname(as_human_readable(selected_files[0])) + os.sep
             ndnam,okay = show_prompt("Directory Name?")
             if okay:
                 numf = 0
@@ -16,7 +20,7 @@ class MoveToDir(DirectoryPaneCommand):
                 if not os.path.isdir(newdirp):
                     os.mkdir(newdirp)
                 for filep in selected_files:
-                    head, tail = os.path.split(filep)
+                    head, tail = os.path.split(as_human_readable(filep))
                     filename = tail or os.path.basename(head)
                     shutil.move(dirPath + filename,newdirp + filename)
                     numf += 1
